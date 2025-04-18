2740fe419bb3cf13e384bc8786e59b2fc389cfb6
kokoa <406817547@qq.com>
2025-04-18 17:49:46 +0800

    rewrite crypto and clear junk code

--- static/geeTest.js
+++ static/geeTest.js
@@ -1,7 +1,7 @@
-const path = require("path")
+const path = require("path");
 const cryptoPath = path.join(process.cwd(), "static", "crypto");
-const aes = require(path.join(cryptoPath, "AES.js"));
-const Md5 = require(path.join(cryptoPath, "md5"));
+const { Aes, CipherMode, PaddingMode } = require(path.join(cryptoPath, "symmetric", "AES.js"));
+const { Md5 } = require(path.join(cryptoPath, "hash", "MD5.js"));
 const jsEncrypt = require(path.join(cryptoPath, "jsEncrypt"));
 
 function e() {
@@ -12,19 +12,18 @@ function guid() {
     return e() + e() + e() + e();
 }
 
-function gen_m(cipher) {
+function gen_m(plaintext) {
     const key = guid();
     const _ = (new jsEncrypt.JsEncrypt()).encrypt(key);
-    const iv = aes.utils.utf8.toBytes("0000000000000000");
-    const aes_cbc = new aes.ModeOfOperation.cbc(aes.utils.utf8.toBytes(key), iv);
-    const cipher_pading = aes.padding.pkcs7.pad(aes.utils.utf8.toBytes(cipher))
-    const aes_encode = aes_cbc.encrypt(cipher_pading);
-    const u = aes.utils.hex.fromBytes(aes_encode);
+    const iv = Buffer.from("0000000000000000", 'utf-8')
+    plaintext = Buffer.from(plaintext, 'utf-8');
+    const aes = new Aes(CipherMode.CBC, PaddingMode.PKCS7);
+    const u = aes.encrypt(plaintext, Buffer.from(key, "utf-8"), iv).toString("hex");
     return u + _
 }
 
 function gen_w(captcha_id, datetime, lot_number, passtime, userresponse) {
-    const md5 = new Md5.Md5();
+    const md5 = new Md5();
     const pow_msg = `1|0|md5|${datetime}|${captcha_id}|${lot_number}||${guid()}`
     let track = {
         "passtime": passtime,
@@ -32,7 +31,7 @@ function gen_w(captcha_id, datetime, lot_number, passtime, userresponse) {
         "device_id": "",
         "lot_number": lot_number,
         "pow_msg": pow_msg,
-        "pow_sign": md5.appendStr(pow_msg).end(),
+        "pow_sign": md5.hash(Buffer.from(pow_msg, 'utf-8')).toString("hex"),
         "geetest": "captcha",
         "lang": "zh",
         "ep": "123",
